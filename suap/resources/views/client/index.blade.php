a8dde9291c7382c92bb8038f7dbf9e3a0979726f
thecoderartist <rodrigo.garciat.se@gmail.com>
2015-10-29 11:46:07 -0600

    Cambios Parciales del modulo de Clientes

---
+++ suap/resources/views/client/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.admin')	
+	@section('content')
+
+
+<div class="col-md-6">
+
+	<div class="form-group">
+      <label for="exampleInputEmail1">Nombre del cliente</label>
+      <input type="text" class="form-control" id="clientName" placeholder="Ingrese el nombre del cliente">
+    </div>
+</div>
+
+
+	@endsection
